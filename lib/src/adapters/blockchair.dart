a446121d7d4e9f650f718a806808d6212052c9ca
Harm Aarts <harm@aarts.email>
2020-01-23 09:32:40 +0100

    Put wait in correct loop

--- lib/src/adapters/blockchair.dart
+++ lib/src/adapters/blockchair.dart
@@ -34,9 +34,8 @@ class Blockchair extends Adapter {
           hash: blockResponse['hash'],
           height: blockResponse['id'],
         );
-
-        await Future.delayed(const Duration(seconds: 5));
       }
+      await Future.delayed(const Duration(seconds: 5));
     }
   }
 
