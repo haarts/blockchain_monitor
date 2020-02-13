352e46d4d7121075700b39db6a4f8a87cf6642be
Harm Aarts <harm@aarts.email>
2020-02-13 13:16:17 +0100

    Add some docs

--- lib/src/blockchain_monitor_base.dart
+++ lib/src/blockchain_monitor_base.dart
@@ -20,16 +20,21 @@ class Monitor {
     ]);
   }
 
+  /// Pass an address to be notified of all transactions occuring involving
+  /// that address.
   Stream<Transaction> address(String address) {
     return RedundantStream<Transaction>(
         _adapters.map((adapter) => adapter.transactions(address))).stream;
   }
 
+  /// Get notified whenever a new block is published.
   Stream<Block> blocks() {
     return RedundantStream<Block>(_adapters.map((adapter) => adapter.blocks()))
         .stream;
   }
 
+  /// Given a transaction hash return a stream with ever increasing
+  /// confirmations counts.
   Stream<int> confirmations(String txHash) {
     return RedundantStream<int>(
         _adapters.map((adapter) => adapter.confirmations(txHash))).stream;
